0127d44856108551744d1bf60ca4b7277f3707ba
trananhtuan23012000 <58499278+trananhtuan23012000@users.noreply.github.com>
2020-11-30 07:29:44 +0700

    Update ARTimeseries3.m

--- ARTimeseries3.m
+++ ARTimeseries3.m
@@ -1,4 +1,4 @@
-function [check,ar,e,arcoeffs,e_check]=ARTimeseries3(n,sigma,alpha)
+function [check,ar,e,arcoeffs,e_check,ar_theory]=ARTimeseries3(n,sigma,alpha)
     % cho hệ số tự tương quan riêng , xây dựng chuỗi {x_t}
     % dự đoán hệ số tự hồi quy
     % n là số lượng điểm muốn sinh ra
@@ -26,36 +26,38 @@ function [check,ar,e,arcoeffs,e_check]=ARTimeseries3(n,sigma,alpha)
          sum=0;
          for k=1:u
              sum =sum + b(u,k)*T(u+1-k);
-             m=m*(1-Alpha(k)^2);
-         end
+        m=m*(1-Alpha(k)^2);
+        end
         T(u+1)= sum + sqrt(sigma*m/mm)*randn(1);
-     end
-     for u=p+1:n-1
-         sum=0;
-         for k=1:p
-             sum =sum + b(p,k)*T(u+1-k);
-         end
+        end
+        for u=p+1:n-1
+        sum=0;
+        for k=1:p
+        sum =sum + b(p,k)*T(u+1-k);
+        end
         T(u+1)= sum + sqrt(sigma)*randn(1);
-     end
-     [r_,lg] = xcorr(T,'biased');
-     r_(lg<0) = [];
-    [ar,e] = levinson(r_,p);
-    [~,~,k] = levinson(r_,100);
-    stem(k,'filled')   
-    conf = sqrt(2)*erfinv(0.95)/sqrt(15000);
-    hold on
-    [X,Y] = ndgrid(xlim,conf*[-1 1]);
-    %[X,Y] = ndgrid(xlim,T);
-    plot(X,Y,'--r')
-    hold off
-    ar(1)=[];
-    check=zeros(10,10);
-    j=1;
-    for i=1:10       
+        end
+        [r_,lg] = xcorr(T,'biased');
+        r_(lg<0) = [];
+        [ar,e] = levinson(r_,p);
+        [~,~,k] = levinson(r_,100);
+        stem(k,'filled') 
+        conf = sqrt(2)*erfinv(0.95)/sqrt(15000);
+        hold on
+        [X,Y] = ndgrid(xlim,conf*[-1 1]);
+        %[X,Y] = ndgrid(xlim,T);
+        plot(X,Y,'--r')
+        hold off
+        ar(1)=[];
+        check=zeros(10,10);
+        j=1;
+        for i=1:10 
         check(i,:)=T(j:j+9,1);
-        j=j+10;       
-    end
-    %hệ số tự hồi quy dự đoán bằng burg
-    [arcoeffs,e_check] = arburg(T,p);
-    arcoeffs(1)=[];
-    end
+        j=j+10; 
+        end
+        [arcoeffs,e_check] = arburg(T,p);
+        arcoeffs(1)=[];
+        ar_theory=zeros(1,p);
+        ar_theory(1,:)=b(p,:);
+        
+end
